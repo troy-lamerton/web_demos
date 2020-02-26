b604761814ca04ed41e817eb720e46810ae8e545
Troy Lamerton <troy.lamerton@gmail.com>
2020-02-26 19:54:29 +0100

    ios jank

--- audio_buffers/main.js
+++ audio_buffers/main.js
@@ -1,20 +1,33 @@
-(function () {
+window.withAudioContext = (audioContext) => {
 
-    window.AudioContext = window.AudioContext || window.webkitAudioContext;
-    var audioContext = new AudioContext();
-    var selectedChannel = 0;
-    var logi = 0;
-    var effect = (function () {
-        var node = audioContext.createScriptProcessor(0, 2, 2);
+    // window.AudioContext = window.AudioContext || window.webkitAudioContext;
+    // const audioContext = new AudioContext();
+    audioContext.addEventListener('statechange', e => {
+        console.log('audio ctx . statechange:', e.currentTarget.state);
+    });
+
+    const vid = document.getElementById('vid');
+
+    let selectedChannel = 0;
+    let logi = 0;
+
+    const effect = (function () {
+        var node = audioContext.createScriptProcessor(undefined, 2, 2);
         console.log('audio node processor', node);
 
         node.addEventListener('audioprocess', function (e) {
+            // get input of the Left of Right channel
             var input = e.inputBuffer.getChannelData(selectedChannel);
+            // we will put the input into one channel and silence the other with zeros
             var outputL = e.outputBuffer.getChannelData(0);
             var outputR = e.outputBuffer.getChannelData(1);
-            if (logi++ % 120 == 0) {
-                console.log('audioprocess the input samples', e);
+
+            if (logi++ % 80 == 0) {
+                if (window.cosAudio) {
+                    window.cosAudio(input);
+                }
             }
+
             for (var i = 0; i < outputR.length; i++) {
                 outputL[i] = selectedChannel == 0 ? input[i] : 0.0;
                 outputR[i] = selectedChannel == 1 ? input[i] : 0.0;
@@ -25,51 +38,46 @@
     })();
 
 
-    var streamAttached = false;
+    let streamAttached = false;
 
-    function attachStream(video) {
+    const source = audioContext.createMediaElementSource(vid);
+
+    const attachStream = () => {
         if (streamAttached) {
             return;
         }
-        var source = audioContext.createMediaElementSource(video);
         source.connect(effect);
         effect.connect(audioContext.destination);
-        console.log('stream was attached ' + !video.paused)
+        console.log('stream was attached ' + !vid.paused)
         streamAttached = true;
-    }
+    };
 
-    console.log();
+    // vid.pause();
+    // attachStream(vid);
 
-    var needtouch = false;
-
-    const vid = document.getElementById('vid');
+    // playback events
+    vid.addEventListener('loadedmetadata', function () {
+        console.log(this, 'loadedmetadata')
+    });
     vid.addEventListener('play', function (e) {
         console.log('onplay lets attach', !e.currentTarget.paused);
         if (streamAttached) {
-            console.log('resume')
-            audioContext.resume()
+            // console.log('resuming audioContext..')
+            // audioContext.resume()
         }
-        attachStream(vid);
-
     });
     vid.addEventListener('pause', function (e) {
-        console.log('suspend!!')
+        console.log('suspending audioContext..')
+        audioContext.suspend()
+    });
+    vid.addEventListener('ended', function (e) {
+        console.warn('video edned.')
         audioContext.suspend()
     });
 
-    vid.addEventListener('loadedmetadata', function () {
-        // this.play();
-        // this.volume=1.0;
-        if (this && this.paused) {
-            if (needtouch == false) {
-                needtouch = true;
-                console.log('need touch attach')
-            }
-        } else {
-            console.log(this, 'should be working, its playing')
-        }
-    });
+    // controls
     window.panToRight = function () {
+        audioContext.resume();
         console.log('panToRight')
         selectedChannel = 1;
     };
@@ -78,4 +86,4 @@
         selectedChannel = 0;
     };
 
-})();
+};
